5b4fb001b75fda075b5cfd3843da3943ee49f797
jundol <pjg120500@gmail.com>
2022-07-22 22:20:51 +0900

    commit

---
+++ 코드/2022-07-22/03_ejs.js
@@ -0,0 +1,3 @@
+var age=23;
+document.write("<div style='color: red; font-size: 24px;'>외부 자바스크립트 파일</div>");
+document.write("당신의 나이는" + age + "입니다.");
---
+++ 코드/2022-07-22/mjs1.js
@@ -0,0 +1,3 @@
+// 외부 스타일을 활용할 수 있음 
+document.write("mjs1현재폴더.js");
+document.write("<div style='color: red; font-size: 24px;'>외부 자바스크립트 파일</div>");
