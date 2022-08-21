968de29edce32864b6e735bd0247a128b120901f
jundol <pjg120500@gmail.com>
2022-08-21 20:50:09 +0900

    찾아오시는길 추가

---
+++ 프로젝트/중간 프로젝트/loc.js
@@ -0,0 +1,48 @@
+// 구글 맵 api
+var myCenter = new google.maps.LatLng(37.50324579472511, 126.87893517055778);
+$(document).ready(function(){
+    myMap()
+});
+function myMap() {
+    var mapProp = {
+        center:myCenter,
+        zoom:17,
+        mapTypeId:google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    var marker = new google.maps.Marker({
+        position:myCenter,
+        animation:google.maps.Animation.BOUNCE
+    });
+    marker.setMap(map);
+}
+
+// 네이버 맵 api
+// var container = document.getElementById('map');
+// var options = { 
+// 	center: new kakao.maps.LatLng(37.503246, 126.878935),
+// 	level: 3,
+//     draggable : false
+// };
+// var map = new kakao.maps.Map(container, options);
+
+// var markerPosition  = new kakao.maps.LatLng(37.503246, 126.878935); 
+// var marker = new kakao.maps.Marker({
+//     position: markerPosition
+// });
+// marker.setMap(map);
+
+// var iwContent = '<div style="border-radius:14px; padding:5px; font-weight:bold;">하이렌트카<br>서울 구로구 경인로 557</div>'
+// var iwPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+// var infowindow = new kakao.maps.InfoWindow({
+//     position : iwPosition, 
+//     content : iwContent 
+// });
+// infowindow.open(map, marker); 
+
+// var mapTypeControl = new kakao.maps.MapTypeControl();
+// map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+// var zoomControl = new kakao.maps.ZoomControl();
+// map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+
